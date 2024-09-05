949a4f01f72b2e514fa1640cfda94e302cb3d048
shanshan <772440958@qq.com>
2024-09-05 14:34:21 +0800

    第三次提交

--- git-learn/src/com/japanwork/LearnGit.java
+++ git-learn/src/com/japanwork/LearnGit.java
@@ -2,6 +2,6 @@ package com.japanwork;
 
 public class LearnGit {
     public static void main(String[] args) {
-        System.out.println("学习代码提交，合并冲突");
+        System.out.println("学习代码提交，合并冲突，制造冲突");
     }
 }
